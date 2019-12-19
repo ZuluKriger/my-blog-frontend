80bac7684c8a2c345763307e4f421719952d39a4
RuneMadsen <webudvikler2020@gmail.com>
2019-12-19 13:36:45 +0100

    front

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const AboutPage = () => (
+    <>
+        <div>
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Expedita consequuntur quibusdam aspernatur reiciendis et iusto placeat
+                repellendus vitae at, necessitatibus esse, quis quod ullam mollitia vero
+                architecto voluptate ut asperiores.
+            </p>
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Expedita consequuntur quibusdam aspernatur reiciendis et iusto placeat
+                    repellendus vitae at, necessitatibus esse, quis quod ullam mollitia vero
+                    architecto voluptate ut asperiores.
+            </p>
+        </div>
+    </>
+)
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <div>
+            <h1>Hello, welcome to my blog!</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Expedita consequuntur quibusdam aspernatur reiciendis et iusto placeat
+                repellendus vitae at, necessitatibus esse, quis quod ullam mollitia vero
+                architecto voluptate ut asperiores.
+            </p>
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Expedita consequuntur quibusdam aspernatur reiciendis et iusto placeat
+                    repellendus vitae at, necessitatibus esse, quis quod ullam mollitia vero
+                    architecto voluptate ut asperiores.
+            </p>
+        </div>
+    </>
+)
+
+export default HomePage;
